08785eab69ff8f948037d4aa112573e5c936d558
heyliceeee <alicedias2002@hotmail.com>
2022-01-24 15:02:13 +0000

    Create trabalho.R

---
+++ trabalho.R
@@ -0,0 +1,104 @@
+#A-0 B-1   #amostra de 21 testes
+
+condutor=c(1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0) #quantitativa nominal (binominal)
+
+peso=c(2035, 1730, 1180, 1530, 1750, 1940, 1460, 1960, 1850, 1533, 1760, 
+         1650, 2050, 1710, 1897, 1380, 1490, 1820, 1510, 1950, 1570) #qualitativa continua
+
+distancia=c(138.9, 181.8, 243.9, 204.1, 178.6, 163.9, 217.4, 163.9, 175.4, 
+              198.2, 178.6, 188.7, 137.0, 185.2, 166.7, 212.8, 221.7, 172.4, 204.1, 151.5, 196.1) #qualitativa contínua
+
+
+#criar tabela  
+table=cbind(condutor, peso, distancia)
+table #mostra tabela
+summary(table)
+
+#tabelas freq aboslutas e relativas
+table(peso) #continua (histograma, bigode)
+table(distancia) #continua
+table(condutor) #var nominal (circular, barras)
+
+#Tabelas
+f_g<-table(condutor) #frequencia absoluta do condutor
+f_g #nº de elementos de cada grupo (frequencia absoluta do condutor)
+fr_g<-prop.table(f_g) #tabelas de forma proporcional da frequencia relativa da frequencia absoluta do condutor 
+fr_g #frequencia relativa por condutor (condutor A -> 57%; condutor B -> 42%)
+
+#circular para freq absoluta ou freq relativa
+#Representações Graficas
+nomes_c<-c("condutor A","condutor B") #legendas
+cores<-c("pink","skyblue") #cores do gráfico
+rotulo<-paste(nomes_c,"(",paste(f_g),")",sep=" ") #dados (legendas, n. elementos)
+pie(f_g, main="Numero de Testes realizados por condutor",labels=rotulo,col=cores) #grafico circular
+
+#Grafico Barras (freq absoluta ou freq relativa) (variáveis nominal e ordenal; Nao serve para var. contínuas!!!!)
+barplot(f_g,main="Numero de Testes realizados por condutor",xlab="condutor",ylab="nº de testes",col=c("pink", "skyblue"), ylim=c(0,15)) #ylim(limites de y) #fa de cada condutor
+barplot(fr_g,main="Percentagem de Testes por condutor",xlab="condutor",ylab="nº de testes",col=c("pink", "skyblue"), ylim=c(0,1)) #ylim(limites de y) #fr de cada condutor
+#text(locator(n=2), paste(round(fr_g, 2))) #introduzir texto no grafico
+
+
+#Histograma
+h<-hist(distancia,main="Distribuição dos testes por distancia",xlab="distancia",ylab="Numero de testes", col="skyblue",xlim=c(0,300),ylim=c(0,10))
+h
+summary(distancia)
+
+freq_rel=h$counts/21 #freq relativa
+#text(locator(n=7), paste(round(freq_rel, 2))) #introduzir texto no grafico
+
+
+#caixa de bigodes multiplo (comparar 2 váriaveis (continua - nominal ou ordinal))
+boxplot(distancia ~ condutor, main = "Comparacao da distancia por condutor", ylab="ordenado em kms", xlab="", names=c("condutor A","condutor B"),col=c("pink","blue"))
+IQR(distancia) #da intervalo interquartil (na distancia, a diferenca entre o 1 qartil e o 3 quartil são 37.4)
+tapply(distancia,condutor,summary)# Para interpretar os valores do boxplot (qt + proximo a media e a mediana estao, mais normais estao (assimetria))
+
+
+#comparar 2 variáveis continuas
+
+#---------------- distancia vai depender do peso do automovel e do condutor ----------------#
+
+# PREVISAO: Regressao linear entre distancia e peso
+plot(peso, distancia, pch = 1, cex = 1.3, col = "blue", main = "peso vs distancia", xlab = "peso", ylab = "distancia")
+# existe uma relacao de correlacao fraca e negativa entre as variaveis "peso" e "distancia"
+cor(peso,distancia) # r= -0.9772903, esta muito longe de 1 por isso existe uma correlacao fraca e negativa
+
+
+# y = B0 + B1 * X + erro <- regressao linear simples
+# peso = B0(=3253.986) + B1(=-8.395) * distancia + erro (se o distancia for nulo(zero), o peso e de 3253kg)
+# ^peso = 3253.986 + -8.395 * distancia (por cada km q se acrescenta á distancia, acrescenta -8.395kg no peso)
+
+
+model = lm(peso ~ distancia) #linear simples #dependente~independente
+model #ver modelo;peso=3253.986+-8.395*distancia
+abline(model, col="red") #desenhar a reta de regressao linear simples #intercept(ordenada da origem); a distancia vai depender do peso do automovel e do condutor
+summary(model) # sumario com as estimativas dos coeficientes, p-value e r-quadrado
+
+#residuals(erros do modelo do y = ... + erro)
+#
+
+#predict(model) #previsao - valores estimados pela reta para cada valor de x dado
+# coeficiente de correlacao e coeficiente de determinacao, para a qualidade do ajustamento
+cor(peso,distancia) # r= -0.9772903
+cor(peso,distancia)^2  # r^2 = 0.9550963  (95,50% da variancia de y e explicada pela variancia de x)
+
+
+###### Estatística INDUTIVA
+#inferencia estatistica
+
+
+#exercicio
+dbinom(8, 19, 0.49) #P(M=8 em 19, 0.49) = 15.25% (a amostra pode n ser representativa)
+
+#t-test a media populacional
+t.test(ord_inic, 
+       alternative="two.sided",
+       mu=1000, #media do ordenado
+       conf.level=0.95) #nivel de confianca #df(grau de liberdade) #pvalue(valor de erro) #interval: ....(1quantil, 2quantil) #como 1000 esta fora de 1000(mean of x)..., entao esta errado
+
+
+t.test(ord_atual, 
+       alternative="two.sided",
+       mu=1000, #media do ordenado
+       conf.level=0.95) #nivel de confianca
+
+
